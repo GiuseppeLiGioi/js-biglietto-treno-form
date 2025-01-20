5514d44e3b59677702a95cd688d4d17ee86ba856
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-01-20 14:42:36 +0100

    ADD: MILESTONE1

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ let etaPasseggero=prompt("inserisci la tua età");
 let kmDaPercorrere=prompt("inserisci i km che devi percorrere");
 const prezzoKm=0.21;
 
+let bottone = document.getElementById("btn");
+
 //FASE DI ELABORAZIONE
 let prezzoTot=kmDaPercorrere * prezzoKm;
 let sconto;
@@ -21,17 +23,24 @@ console.log(`${prezzoTot}`);
 
 
 //FASE DI OUTPUT
-if(etaPasseggero < 18){
-sconto= (prezzoTot * 20) / 100;
-prezzoScontato = prezzoTot - sconto;
+//MILESTONE 1:
 
-console.log(`visto che sei minorenne hai diritto al 20% di sconto, ecco il prezzo scontato! ${prezzoScontato.toFixed(2)} euro. Altrimenti avresti pagato ${prezzoTot.toFixed(2)} euro.`)
-}
-else if(etaPasseggero > 65){
-sconto= (prezzoTot * 40) / 100;
-prezzoScontato = prezzoTot - sconto;
-console.log(`visto che hai più di 65 anni, hai diritto al 40% di sconto, ecco il prezzo scontato! ${prezzoScontato.toFixed(2)} euro. Altrimenti avresti pagato ${prezzoTot.toFixed(2)} euro.`)
-}
-else{
-    console.log(`il prezzo ammonta a: ${prezzoTot.toFixed(2)} euro.`);
-}
+bottone.addEventListener("click" , function(){
+  if(etaPasseggero < 18){
+    sconto= (prezzoTot * 20) / 100;
+    prezzoScontato = prezzoTot - sconto;
+    
+    console.log(`visto che sei minorenne hai diritto al 20% di sconto, ecco il prezzo scontato! ${prezzoScontato.toFixed(2)} euro. Altrimenti avresti pagato ${prezzoTot.toFixed(2)} euro.`)
+    }
+    else if(etaPasseggero > 65){
+    sconto= (prezzoTot * 40) / 100;
+    prezzoScontato = prezzoTot - sconto;
+    console.log(`visto che hai più di 65 anni, hai diritto al 40% di sconto, ecco il prezzo scontato! ${prezzoScontato.toFixed(2)} euro. Altrimenti avresti pagato ${prezzoTot.toFixed(2)} euro.`)
+    }
+    else{
+        console.log(`il prezzo ammonta a: ${prezzoTot.toFixed(2)} euro.`);
+    }
+})
+
+
+//MILESTONE 2
